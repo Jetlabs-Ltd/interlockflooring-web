eae2c57fe3077560b220d3415b87b15364d38baa
William Adjei <will@pixellabs.com>
2023-03-11 23:30:13 +0000

    initial commit from live backup

---
+++ web/about.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+        "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="en">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=iso-8859-1">
+	<title>About Interlock Flooring</title>
+	<link rel="Stylesheet" href="styles.css" type="text/css">
+</head>
+<body>
+<table width="80%">
+	<tr>
+		<td rowspan="2"><img src="images/spacer.gif" width="25" border="0"></td>
+		<td><img src="images/spacer.gif" height="20" border="0"></td>
+	</tr>
+	<tr>
+		<td>
+			<p class="titletext">About Interlock Flooring</p>
+			
+			<p class="maintext">Interlock flooring are distributors of laminated flooring for the West Africa
+				region.</p>
+			<p class="maintext">We also provide an expert fitting service within Nigeria, using materials
+			of the highest quality.</p>
+			
+	</tr>	 
+</span>
+</body>
+</html>
---
+++ web/contact.php
@@ -0,0 +1,90 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+        "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="en">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=iso-8859-1">
+	<title>About Interlock Flooring</title>
+	<link rel="Stylesheet" href="styles.css" type="text/css">
+</head>
+<body>
+<table width="80%">
+	<tr>
+		<td rowspan="2"><img src="images/spacer.gif" width="25" border="0"></td>
+		<td><img src="images/spacer.gif" height="20" border="0"></td>
+	</tr>
+	<tr>
+		<td>
+			<p class="titletext">Contact Information</p>
+			
+			<table border="0" cellspacing="5">
+				<tr valign="top">
+					<td class="titletext" colspan="2">Company Details</td>
+				</tr>
+				<tr valign="top">
+					<td class="maintext" align="right">
+						Address:</td>
+					<td class="maintext" align="left">
+						Interlock Flooring<br>
+						68 Old Yaba Road<br>
+						Ebute Metta<br>
+						Lagos<br>
+						Nigeria<br></td>
+				</tr>
+				<tr valign="top">
+					<td class="maintext" align="right">
+						Telephone:</td>
+					<td class="maintext" align="left">
+						+234 (0) 1774 5699<br></td>
+				</tr>
+				<tr valign="top">
+					<td class="maintext" align="right">
+						E-Mail:</td>
+					<td class="maintext" align="left">
+						<a class="maintext" href="mailto:info@interlockflooring.com">info@interlockflooring.com</a></td></tr>
+			</table>
+		</td>
+	</tr>
+	<tr>
+		<td rowspan="2"><img src="images/spacer.gif" width="25" border="0"></td>
+		<td><img src="images/spacer.gif" height="20" border="0"></td>
+	</tr>
+	<tr>
+		<td>
+			<table border="0" cellspacing="8">
+				<tr valign="top">
+					<td class="titletext" colspan="2">People</td>
+				</tr>
+				<tr valign="top">
+					<td class="maintext" align="left">
+						<b>Chief E L OLAAWO</b> finiqs, fias, aciarb
+						<br>
+						Chairman
+					</td>
+				</tr>
+				<tr valign="top">
+					<td class="maintext" align="left">
+						<b>Feyi OLAAWO</b>
+						<br>
+						Regional Manager
+					</td>
+				</tr>
+				<tr valign="top">
+					<td class="maintext" align="left">
+						<b>Olawale OLAWO</b>
+						<br>
+						Managing Director / C.T.O
+					</td>
+				</tr>
+				<tr valign="top">
+					<td class="maintext" align="left">
+						<b>Edward SERIKI</b>
+						<br>
+						Director / C.E.O.
+					</td>
+				</tr>
+			</table>
+		</td>
+	</tr>
+</table>
+</body>
+</html>
